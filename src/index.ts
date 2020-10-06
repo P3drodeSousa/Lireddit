6fe1507da8e94e9997afcd865113ce2d065a14dc
tempname1533 <pedro_de_sousa@outlook.com>
2020-10-06 13:07:04 +0200

    Mikrorm setuo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import { Post } from "./entities/Post";
+import { __prod__ } from "./contants";
+import { MikroORM } from "@mikro-orm/core";
+import { url } from "inspector";
+import { type } from "os";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "xhtpizfm",
+    user: "xhtpizfm",
+    password: "ioNZ8YQorVREwxkE-N1x3Ew0dHtXu_t3",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+
+  const post = orm.em.create(Post, { title: "my frist post" });
+};
